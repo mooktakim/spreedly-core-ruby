24e966afe9b6d83ee372bca89b5f228a8396d864
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2012-10-11 12:49:39 +0100

    pass through order_id and description

--- lib/spreedly-core-ruby/payment_method.rb
+++ lib/spreedly-core-ruby/payment_method.rb
@@ -129,7 +129,9 @@ module SpreedlyCore
           :currency_code => currency,
           :ip => options[:ip_address],
           :redirect_url => options[:redirect_url],
-          :callback_url => options[:callback_url]
+          :callback_url => options[:callback_url],
+          :order_id => options[:order_id],
+          :description => options[:description]
         }
       }
       self.class.verify_post(path, :body => data,
